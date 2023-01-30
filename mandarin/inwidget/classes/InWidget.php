59d4995134db6a971241b6b54f6598e999c32a40
bastetka <34072501+bastetka@users.noreply.github.com>
2023-01-30 14:51:01 +0300

    Delete mandarin directory

--- mandarin/inwidget/classes/InWidget.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require_once __DIR__ . '/InWidget/Exception/InWidgetException.php';
-require_once __DIR__ . '/InWidget/Api/ApiModel.php';
-require_once __DIR__ . '/InWidget/Api/ApiScraper.php';
-require_once __DIR__ . '/InWidget/Api/ApiOfficial.php';
-require_once __DIR__ . '/InWidget/Core.php';
