5871ff0129a296cd07edd1ad137c1bb9de624da6
bastetka <34072501+bastetka@users.noreply.github.com>
2023-02-15 11:39:10 +0300

    Add files via upload

--- Harant_sans/pages/lawyer/js/lawyer.js
+++ Harant_sans/pages/lawyer/js/lawyer.js
@@ -113,6 +113,14 @@ jQuery(function ($) {
 				infinite: false,
 				slidesToShow: 3,
 				slidesToScroll: 1,
+        responsive: [
+          {
+            breakpoint: 1366,
+            settings: {
+              slidesToShow: 2,
+            },
+          }
+        ]
 			});
 		}
 	}
